a08823d4d18bcef50ea16ff073a7d7d9a8f15622
Dannyblazer <nnajidanny004@gmail.com>
2023-05-27 20:28:29 +0100

    Models implementation.

--- todos/models.py
+++ todos/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
 class Todo(models.Model):
-    # user = models.ForeignKey
-    content = models.CharField(max_length=50, unique=False)
-    timer   = models.DateField()
-    created_at = models.DateField(auto_now_add=True)
-    priority = models.CharField(max_length=25, unique=False)
+    PRIORITY_CHOICES = (
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.content
+        return self.title
 
 
