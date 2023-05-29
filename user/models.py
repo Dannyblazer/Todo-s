4786f9560c4da14ef3174b27a398df0ef3a527d7
Dannyblazer <nnajidanny004@gmail.com>
2023-05-29 20:54:46 +0100

    cleared unnecessary comments.

--- user/models.py
+++ user/models.py
@@ -41,7 +41,6 @@ class User(AbstractBaseUser):
 	is_active				= models.BooleanField(default=True)
 	is_staff				= models.BooleanField(default=False)
 	is_superuser			= models.BooleanField(default=False)
-	# first_name 				= models.CharField(max_length=30)
 
 	USERNAME_FIELD = 'email'
 	REQUIRED_FIELDS = ['username',]
