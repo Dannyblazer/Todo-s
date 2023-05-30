ddc74d013cd926904051d3df93731f835d3bde46
Dannyblazer <nnajidanny004@gmail.com>
2023-05-30 21:03:51 +0100

    Todo model changes.

--- todos/models.py
+++ todos/models.py
@@ -11,7 +11,7 @@ class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES)
+    priority = models.CharField(max_length=15, choices=PRIORITY_CHOICES)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
