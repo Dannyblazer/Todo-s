2885cbfe1757e3367a1b37b5e928e7052a7a543b
Dannyblazer <nnajidanny004@gmail.com>
2023-09-09 11:18:20 +0100

    edited the forms

---
+++ todos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from todos.views import *
+app_name = 'todos'
+
+urlpatterns = [
+    path('create/', create_todo, name='create'),
+    path('update/', edit_task, name='edit'),
+  ]
