0d701825c28a338369f999de28ada6e46ecc0e7c
Dannyblazer <nnajidanny004@gmail.com>
2024-04-07 15:01:52 +0100

    user model refactored

--- users/models.py
+++ users/models.py
@@ -1,78 +1,69 @@
-
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.conf import settings
-from django.db.models.signals import post_save
-
-
-
+from django.utils.translation import gettext_lazy as _
 
 class MyAccountManager(BaseUserManager):
-	def create_user(self, email, username, password=None):
-		if not email:
-			raise ValueError("Users must have an email address")
-		if not username:
-			raise ValueError("Users must have an username")
+    def create_user(self, email, username, password=None, **extra_fields):
+        """
+        Create and return a regular user with given email, username, and password.
 
-		user  = self.model(
-				email=self.normalize_email(email),
-				username=username,
-			)
+        Raises:
+            ValueError: If email or username is not provided.
+        """
+        if not email:
+            raise ValueError(_("Users must have an email address"))
+        if not username:
+            raise ValueError(_("Users must have a username"))
 
-		user.set_password(password)
-		user.save(using=self._db)
-		return user
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-	def create_superuser(self, email, username, password):
-		user  = self.create_user(
-				email=self.normalize_email(email),
-				password=password,
-				username=username,
-			)
-		user.is_admin = True
-		user.is_staff = True
-		user.is_superuser = True
-		user.save(using=self._db)
-		return user
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        """
+        Create and return a superuser with given email, username, and password.
 
+        Raises:
+            ValueError: If is_staff or is_superuser is not True.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self.create_user(email, username, password, **extra_fields)
 
 class Account(AbstractBaseUser):
-	email 					= models.EmailField(verbose_name="email", max_length=60, unique=True)
-	username 				= models.CharField(max_length=30, unique=True)
-	date_joined				= models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-	last_login				= models.DateTimeField(verbose_name='last login', auto_now=True)
-	is_admin				= models.BooleanField(default=False)
-	is_active				= models.BooleanField(default=True)
-	is_staff				= models.BooleanField(default=False)
-	is_superuser			= models.BooleanField(default=False)
-	# first_name 				= models.CharField(max_length=30)
+    email = models.EmailField(_("email address"), max_length=60, unique=True)
+    username = models.CharField(_("username"), max_length=30, unique=True)
+    date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
+    last_login = models.DateTimeField(_("last login"), auto_now=True)
+    is_active = models.BooleanField(_("active"), default=True)
+    is_staff = models.BooleanField(_("staff status"), default=False)
+    is_admin = models.BooleanField(_("admin status"), default=False)
+    is_superuser = models.BooleanField(_("superuser status"), default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username',]
+    objects = MyAccountManager()
 
-	objects = MyAccountManager()
+    class Meta:
+        verbose_name = _("account")
+        verbose_name_plural = _("accounts")
 
-	def __str__(self):
-		return self.email
+    def __str__(self):
+        return f"{self.email}"
 
-	def has_perm(self, perm, obj=None):
-		return self.is_admin
+    def has_perm(self, perm, obj=None):
+        """Return True if the user has the specified permission."""
+        return self.is_staff
 
-	def has_module_perms(self, app_label):
-		return True
-	
-
-'''class Profile(models.Model):
-	account = models.OneToOneField(Account, on_delete=models.CASCADE)
-	follower = models.ForeignKey(Account, related_name="owner", on_delete=models.CASCADE)
-	image = models.ImageField(upload_to=upload_location, null=True, blank=True)
-	date_of_birth = models.DateField()
-	first_name = models.CharField(max_length=30)
-	last_name = models.CharField(max_length=30)
-	Bio = models.CharField(max_length=250)
-
-
-	def __str__(self):
-		return str(self.first_name + " " + self.last_name)
-
-'''
+    def has_module_perms(self, app_label):
+        """Return True if the user has permissions to view the app 'app_label'."""
+        return self.is_staff
