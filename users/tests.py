374f83dc1e2d10599cc2e96fa09b972b6682158b
Dannyblazer <nnajidanny004@gmail.com>
2024-04-07 14:50:11 +0100

    users app test

--- users/tests.py
+++ users/tests.py
@@ -4,63 +4,33 @@ from django.test import TestCase
 # Create your tests here.
 
 class UserManagerTests(TestCase):
-
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(email='normal@user.com', password='foo')
-        self.assertEqual(user.email, 'normal@user.com')
+    
+    def test_create_user(self):# username is None for the AbstractUser option
+        # username does not exist for the AbstractBaseUser option
+        User = get_user_model() 
+        user = User.objects.create_user(username='normal', email='daniel@user.com', password='foo')
+        self.assertEqual(user.email, 'daniel@user.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(user.username)
-        except AttributeError:
-            pass
+        self.assertEqual(user.username, 'normal')  # Check if username is set correctly
         with self.assertRaises(TypeError):
             User.objects.create_user()
         with self.assertRaises(TypeError):
             User.objects.create_user(email='')
         with self.assertRaises(ValueError):
-            User.objects.create_user(email='', password="foo")
+            User.objects.create_user(username='', email='', password="foo")
+
     
     def test_create_superuser(self):
         User = get_user_model()
-        admin_user = User.objects.create_superuser(email='super@user.com', password='foo')
+        admin_user = User.objects.create_superuser(username='super', email='super@user.com', password='foo')
         self.assertEqual(admin_user.email, 'super@user.com')
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
-        try:
-            # username is None for the AbstractUser option
-            # username does not exist for the AbstractBaseUser option
-            self.assertIsNone(admin_user.username)
-        except AttributeError:
-            pass
+        self.assertEqual(admin_user.username, 'super')  # Check if username is set correctly
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
-                email='super@user.com', password='foo', is_superuser=False)
+                username='super', email='super@user.com', password='foo', is_superuser=False)
             
-
-"""
-def login_view(request):
-    def login_request(request):
-        if request.method == "POST":
-            form = RegistrationForm(request, data=request.POST)
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
-                user = authenticate(username=username, password=password)
-                if user is not None:
-                    login(request, user)
-                    messages.info(request, f"You're now logged in as { username }.")
-                    return HttpResponseRedirect(reverse('index'))
-                else:
-                    messages.error(request, "Invalid username or password.")
-            else:
-                messages.error(request, "Invalid username or password.")
-        form = RegistrationForm()
-        return render(request=request, template_name="users:login.html", context={"form":form})
-
-"""
