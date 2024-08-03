9f84818aa88e5aaebb7c831a8aa27263db1aef1f
Dannyblazer <nnajidanny004@gmail.com>
2024-08-03 11:00:14 +0100

    edited settings to use .env file

--- Todo/settings.py
+++ Todo/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import zoneinfo
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -21,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_58lu7()5x*u^10++feh-&!v16w9%15%-mc-ny*(4kplxull(j'
-
+SECRET_KEY = str(os.getenv('SECRET_KEY', None))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
